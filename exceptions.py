e54d96fd26ab37ab84cf486ea2cf3048a49d0b5f
Patricia-Buchner <pbuchner.santos@gmail.com>
2022-04-06 18:58:30 +0200

    Added exceptions

--- exceptions.py
+++ exceptions.py
@@ -24,65 +24,80 @@
 
 # AttributeError - EXAMPLE
 def produce_attribute_error():
-    # print(1.234.upper())
+    print(1.234.upper())
     pass
 
 
 # KeyError
 def produce_key_error():
+    my_dict = { "name": "Patricia" }
+    print(my_dict["age"])
     pass
 
 
 # IndexError
 def produce_index_error():
+    my_list = [1,2,3]
+    my_list[100]
     pass
 
 
 # NameError
 def produce_name_error():
+    return my_var
     pass
 
 
 # UnboundLocalError
 def produce_unbound_local_error():
+    print(f"{my_var}")
+    my_var = 10
     pass
 
 
 # TypeError
 def produce_type_error():
+    print("dfgdfs" + 3)
     pass
 
 
 # ValueError
 def produce_value_error():
+    int("abc")
     pass
 
 
 # ZeroDivisionError
 def produce_zero_division_error():
+    100/0
     pass
 
 
 # OverflowError
 def produce_overflow_error():
+    10.0 ** 9999999
     pass
 
 
 # FileNotFoundError
 def produce_file_not_found_error():
+    open("sdfdsf.txt", "r")
     pass
 
 
 # UnicodeEncodeError
 def produce_unicode_encode_error():
+    "ä".encode("ascii") # UTF-8
     pass
 
 
 # ModuleNotFoundError
 def produce_module_not_found_error():
+    import dfsalkdf
     pass
 
 
 # ImportError
 def produce_import_error():
+    from random import kdjflsdjf
     pass
